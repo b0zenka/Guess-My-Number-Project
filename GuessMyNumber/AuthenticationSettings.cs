d18deb0f9f12c8a921660c2ebc4d1485d956e7bf
b0zenamazur <mazur.bozena94@gmail.com>
2022-03-20 16:48:31 +0100

    init

---
+++ GuessMyNumber/AuthenticationSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace GuessMyNumber
+{
+    public class AuthenticationSettings
+    {
+        public string JwtKey { get; set; }
+        public int JwtExpireDays { get; set; }
+        public string JwtIssuer { get; set; }
+    }
+}
---
+++ GuessMyNumber/Controllers/GameController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuessMyNumber.Controllers
+{
+    public class GameController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Start()
+        {
+            return View();
+        }
+    }
+}
