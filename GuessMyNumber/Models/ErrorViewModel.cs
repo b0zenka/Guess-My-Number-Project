c6215a7849db305f0c851a79b3c37e554ebd5889
b0zenamazur <mazur.bozena94@gmail.com>
2022-06-12 09:54:02 +0200

    delete unuseful code

--- GuessMyNumber/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GuessMyNumber.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
